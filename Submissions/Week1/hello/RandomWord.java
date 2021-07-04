b226c6f042beee3b09100926cff24edc0ed1fc1a
Jiayin Li <jiayin.li007@outlook.com>
2021-07-04 19:51:57 -0400

    Assignment 0

---
+++ Submissions/Week1/hello/RandomWord.java
@@ -0,0 +1,31 @@
+/* *****************************************************************************
+ *  Name:              Ada Lovelace
+ *  Coursera User ID:  123456
+ *  Last modified:     October 16, 1842
+ **************************************************************************** */
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class RandomWord {
+    public static void main(String[] args) {
+        int count = 1;
+        String champion = StdIn.readString();
+
+        if (!champion.equals("")) {
+
+            while (!StdIn.isEmpty()) {
+                count++;
+                String temp = StdIn.readString();
+                if (!StdRandom.bernoulli(1.0 / count)) {
+                    champion = temp;
+                }
+            }
+
+        }
+
+        StdOut.println(champion);
+
+    }
+}
