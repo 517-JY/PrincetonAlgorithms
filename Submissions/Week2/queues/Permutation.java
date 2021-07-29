0c4c01484460533554e20d20d5df245e5a462311
Jiayin Li <jiayin.li007@outlook.com>
2021-07-29 13:08:22 -0400

    Permutation implementation

---
+++ Submissions/Week2/queues/Permutation.java
@@ -0,0 +1,37 @@
+/* *****************************************************************************
+ *  Name: Jiayin Li
+ *  Date: 210728
+ *  Description: Permutation implementation
+ **************************************************************************** */
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Permutation {
+    public static void main(String[] args) {
+        int k = Integer.parseInt(args[0]);
+        RandomizedQueue<String> queue = new RandomizedQueue<String>();
+        // int i = 0;
+        int j = 0;
+
+        while (!StdIn.isEmpty()) {
+            String s = StdIn.readString();
+            queue.enqueue(s);
+            // i++;
+            // StdOut.print(" i = " + i + " ");
+            // StdOut.println("s = " + s);
+        }
+
+        // StdOut.println("Size of queue is " + queue.size());
+
+        for (String s : queue) {
+            StdOut.println(s);
+            j++;
+            if (j == k) {
+                break;
+            }
+        }
+
+
+    }
+}
